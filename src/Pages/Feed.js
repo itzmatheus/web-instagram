156c15b0ffb6ed876e2db2a5f817e9811ef0f967
Matheus Leite <matheusjoselfm@gmail.com>
2019-11-18 00:21:39 -0300

    Add projeto inicial e estruturado com tela de feed e conexao com api

---
+++ src/Pages/Feed.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import api from '../services/api';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
+import './Feed.css';
+import Axios from 'axios';
+
+class Feed extends Component {
+
+    state = {
+        feed: [],
+    };
+    
+    async componentDidMount() {
+        // Metodo executado automaticamente quando o componente for montado  em tela
+        const response = await api.get('posts');
+
+        this.setState({ feed: response.data });
+    }
+
+    render() {
+        return (
+            <section id="post-list">
+
+                { this.state.feed.map(post => (
+                    <article key={post._id}>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
+
+                            <img src={more} alt="Mais" />
+                        </header>
+
+                        <img src={`http://localhost:8080/files/${post.image}`} alt="Foto Perfil"/>
+
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt="" />
+                                <img src={comment} alt="" />
+                                <img src={send} alt="" />
+                            </div>
+
+                            <strong>{post.likes} curtidas</strong>
+
+                            <p>
+                            {post.description}
+                            <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>   
+                )) }
+
+            </section>
+        )
+    };
+}
+
+export default Feed;
